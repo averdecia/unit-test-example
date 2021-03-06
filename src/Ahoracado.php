e184d27c0ff6de3c27b7eedc3d5632bd7e64bfa2
Amet Martinez Verdecia <martineza@MacBook-Pro-de-Amet-Martinez.local>
2021-03-06 14:15:22 -0600

    initial commit

---
+++ src/Ahoracado.php
@@ -0,0 +1,37 @@
+<?php
+
+class Ahorcado{
+
+    private $maxTry;
+    private $word;
+    private $leftWords;
+
+    private function __construct($word, $maxTry = 3)
+    {
+        $this->maxTry = $maxTry;
+        $this->word = $word;
+        $this->leftWords = $this->word;
+    }
+
+    public function ended(): bool {
+        return $this->maxTry === 0 || strlen($this->leftWords) === 0;
+    }
+
+    public function addPlay($letter): void {
+        if($this->ended()) return;
+
+        $startlen = strlen($this->leftWords);
+        $newWords = str_replace($letter, "", $this->leftWords);
+
+        if( $startlen ===  strlen($newWords)){
+            // No changes
+            $this->maxTry -= 1;
+        } else {
+            $this->leftWords = $newWords;
+        }
+    }
+
+    public function win(){
+        return $this->maxTry !== 0 || strlen($this->leftWords) === 0;
+    }
+}
---
+++ tests/AhorcadoTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase{
+
+    public function testAhorcadoEndedTrue(): void{
+        $game = new Ahorcado("casas", 3);
+        $game->addPlay("c");
+        $this->assertFalse($game->ended());
+        $game->addPlay("a");
+        $this->assertFalse($game->ended());
+        $game->addPlay("s");
+        $this->assertTrue($game->ended());
+    }
+
+    public function testAhorcadoEndedFalse(): void{
+        $game = new Ahorcado("casas", 3);
+        $game->addPlay("b");
+        $this->assertFalse($game->ended());
+        $game->addPlay("f");
+        $this->assertFalse($game->ended());
+        $game->addPlay("c");
+        $this->assertTrue($game->ended());
+    }
+
+    public function testAhorcadoWin(): void{
+        $game = new Ahorcado("casas", 3);
+        $game->addPlay("c");
+        $game->addPlay("a");
+        $game->addPlay("s");
+        $this->assertTrue($game->win());
+    }
+}
